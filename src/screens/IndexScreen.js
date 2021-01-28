8091201a1380a218677f57874b7494b3665bd90e
bryan kretz <kretzbryan@gmail.com>
2021-01-28 11:46:03 -0800

    style added to IndexScreen.js. Trash icon added to IndexScreen.js

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,21 +1,43 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-	const { data, addBlogPost } = useContext(BlogContext);
+	const { state, addBlogPost } = useContext(Context);
 	return (
 		<View>
 			<Text>This is an index Screen</Text>
 			<Button title='Add Post' onPress={() => addBlogPost()} />
 			<FlatList
-				data={data}
+				data={state}
 				keyExtractor={(post) => post.title}
-				renderItem={({ item }) => <Text>{item.title}</Text>}
+				renderItem={({ item }) => {
+					return (
+						<View style={styles.row}>
+							<Text style={styles.titleStyle}>{item.title}</Text>
+							<Feather style={styles.iconStyle} name='trash' />
+						</View>
+					);
+				}}
 			/>
 		</View>
 	);
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	row: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingVertical: 20,
+		borderTopWidth: 1,
+		paddingHorizontal: 10,
+	},
+	titleStyle: {
+		fontSize: 18,
+	},
+	iconStyle: {
+		fontSize: 24,
+	},
+});
 
 export default IndexScreen;
