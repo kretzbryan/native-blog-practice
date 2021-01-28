4c43aecc1582e36d50777cc3b4dfc6de50948691
bryan kretz <kretzbryan@gmail.com>
2021-01-27 22:32:06 -0800

    Index screen added.BlogContext added to App.js

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+	return (
+		<View>
+			<Text>This is an index Screen</Text>
+		</View>
+	);
+};
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
